ceb5e3bff8df374dba404af13506003c7b443c63
NitinR99 <rames11a@uwindsor.ca>
2021-06-24 14:34:55 -0400

    pagination and products added to browse.php

--- browse.php
+++ browse.php
@@ -18,45 +18,103 @@
 		<div class="row">
 			<div class="col-lg-12 mb-4 mt-2 text-center">
             <h2>Browse</h2>
-      </div>
+      </div></div>
       <?php
-	$query = $_GET['category'];
+      // Initialize the session
+      session_start();
+      // Include config file
+      require_once "config.php";
+      //set page number
+      if (!isset ($_GET['page']) ) {
+        $page = 1;
+      } else {
+        $page = $_GET['page'];
+      }
+
+	$cat = $_GET['category'];
+  $curr_page=$_GET['page'];
+  if($curr_page == null){
+    $curr_page=1;
+  }
 	// gets value sent over search form
-  $query = htmlspecialchars($query);
+  $cat = htmlspecialchars($cat);
 		// changes characters used in html to their equivalents, for example: < to &gt;
     //if else to get name of the category
-    if($query == 101)
+    $category="";
+    if($cat == 101)
     {
-      $query="Cell Phones";
+      $category="Cell Phones";
     }
-    else if($query == 102)
+    else if($cat == 102)
     {
-      $query="Headphones & Speakers";
+      $category="Headphones & Speakers";
     }
-    else if($query == 103)
+    else if($cat == 103)
     {
-      $query="Cameras";
+      $category="Cameras";
     }
-    else if($query == 104)
+    else if($cat == 104)
     {
-      $query="TV & Home Theatres";
+      $category="TV & Home Theatres";
     }
-    else if($query == 105)
+    else if($cat == 105)
     {
-      $query="Wearable Technology";
+      $category="Wearable Technology";
     }
-    else if($query == 106)
+    else if($cat == 106)
     {
-      $query="Smart Home";
+      $category="Smart Home";
     }
     else {
-      header("location: categories.php");
-      exit;
+    //  header("location: categories.php");
+    //  exit;
     }
-  echo "<p>Viewing ".$query." products";
+  echo "<p>Viewing ".$category." products <br/><hr/>";
+
+  //getting data a page
+  $results_per_page = 9;
+  $page_first_result = ($page-1) * $results_per_page;
+
+  $query = "select * from products where p_category=".$cat;
+  $result = mysqli_query($link, $query);
+  $number_of_result = mysqli_num_rows($result);
+
+  //determine the total number of pages available
+  $number_of_page = ceil ($number_of_result / $results_per_page);
+
+
+
+
+  $query = "SELECT p_name, p_image FROM products where p_category=".$cat." LIMIT " . $page_first_result . ',' . $results_per_page;  //to be changed
+  $result = mysqli_query($link, $query);
+
+  echo '<div class="row">';
+  while ($row = mysqli_fetch_array($result)) { //printing products on screen
+    echo ' <div class="col-sm-4">
+              <div class="card" style="height: 300px">
+                <img class="card-img-top" src="'.$row['p_image'].'" style=" display: block; margin-left: auto; margin-right: auto;max-width: 150px; max-height:250px; overflow: hidden; object-position: 50% 50%; object-fit: contain;" alt="'.$row['p_name'].'">
+                <h5 class="card-title">'.$row['p_name'].'</h5>
+              </div>
+            </div>';
+  }
+  echo '</div></br><hr/>';
+  echo '<div>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">';
+  for($page = 1; $page<= $number_of_page; $page++) { //pagination
+    if($curr_page ==$page){
+               $isActive="active";
+    }
+    else{
+      $isActive="";
+    }
+    echo '<li class="page-item '.$isActive.' "><a class="page-link" href = "browse.php?category='. $cat .'&page=' . $page . '">' . $page . ' </a></li>'; }
+    echo '</ul>
+      </nav>
+    </div>';
 ?>
 		</div>
-      </div>
+
 
 		<div>
 	<?php include($IPATH."footer.html"); ?> <!-- Footer -->
