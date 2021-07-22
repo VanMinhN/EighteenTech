c0580ade6aa1f43e79cb700dbeb9f80c27fa3717
NitinR99 <rames11a@uwindsor.ca>
2021-07-22 16:53:04 -0400

    added form for adding new product, yet to implement functionality

--- admin/adminProductManagement.php
+++ admin/adminProductManagement.php
@@ -2,6 +2,91 @@
 <div class="container">
 	<h2>Product management</h2>
   <hr>
+
+	<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#addNewProduct">
+		Add new product
+	</button>
+	<div class="modal" id="addNewProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog-lg modal-dialog-centered" style="margin-left:20%; margin-right: 20%" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Add new product</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+			<form>
+
+      <div class="modal-body">
+				<div class="row">
+    			<div class="col">
+						<label for="newProductName">Product Name</label>
+						<div class="input-group mb-3">
+							<input type="text" class="form-control" id="newProductName" >
+						</div>
+    			</div>
+    			<div class="col">
+						<label for="newProductImage">Product Image Link</label>
+		  			<div class="input-group mb-3">
+							<div class="input-group-prepend">
+		    				<span class="input-group-text" id="newProductImage1">🔗</span>
+		  				</div>
+		    			<input type="text" class="form-control" id="newProductImage" aria-describedby="newProductImage1">
+		  			</div>
+    			</div>
+  			</div>
+
+				<label for="newProductDescription">Product Description</label>
+				<div class="input-group">
+  				<textarea id="newProductDescription" class="form-control"></textarea>
+				</div>
+				<br>
+				<label for="newProductSpecs">Product Specifications</label>
+				<div class="input-group">
+  				<textarea id="newProductSpecs" class="form-control"></textarea>
+				</div>
+				<br>
+				<div class="row">
+    			<div class="col">
+						<label for="newProductAmazon">Amazon Link</label>
+		  			<div class="input-group mb-3">
+							<div class="input-group-prepend">
+		    				<span class="input-group-text" id="newProductAmazon1">🔗</span>
+		  				</div>
+		    			<input type="text" class="form-control" id="newProductAmazon" aria-describedby="newProductAmazon1">
+		  			</div>
+					</div>
+					<div class="col">
+						<label for="newProductBestBuy">BestBuy Link</label>
+		  			<div class="input-group mb-3">
+							<div class="input-group-prepend">
+		    				<span class="input-group-text" id="newProductBestBuy1">🔗</span>
+		  				</div>
+		    			<input type="text" class="form-control" id="newProductBestBuy" aria-describedby="newProductBestBuy1">
+		  			</div>
+					</div>
+					<div class="col">
+						<label for="newProductNewegg">Newegg Link</label>
+		  			<div class="input-group mb-3">
+							<div class="input-group-prepend">
+		    				<span class="input-group-text" id="newProductNewegg1">🔗</span>
+		  				</div>
+		    			<input type="text" class="form-control" id="newProductNewegg" aria-describedby="newProductNewegg1">
+		  			</div>
+					</div>
+				</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+		</form>
+
+    </div>
+  </div>
+</div>
+
+	<hr>
   <table class="table table-hover">
   <thead>
     <tr>
