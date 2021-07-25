7db7da0d3f1ca3555e34ebe7fd5dd40ecdf66812
bsohail <bmsohail@hotmail.com>
2021-07-24 22:30:59 -0400

    Worked on Carousel and admin features

--- admin.php
+++ admin.php
@@ -3,15 +3,15 @@
 session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+  header("location: login.php");
+  exit;
 }
-if ($_SESSION["is_admin"] != true){
+if ($_SESSION["is_admin"] != true) {
   header("location: user.php");
   exit;
 }
-if (!isset ($_GET['page']) ) {
+if (!isset($_GET['page'])) {
   $page = "admininfo";
 } else {
   $page = $_GET['page'];
@@ -21,49 +21,67 @@ if (!isset ($_GET['page']) ) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>EighteenTech</title>
   <meta charset="UTF-8">
-    		<meta name="description" content="Technology reviews">
-    		<meta name="keywords" content="appliances, tech, review, tv, mobile, headphone, laptop, phone">
-    		<meta name="author" content="Nitin Ramesh">
-   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="description" content="Technology reviews">
+  <meta name="keywords" content="appliances, tech, review, tv, mobile, headphone, laptop, phone">
+  <meta name="author" content="Nitin Ramesh">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
   <div>
-  <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/php/"; include($IPATH."navbar.php"); ?> <!-- Navbar -->
-</div>
-	<!--
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
+    include($IPATH . "navbar.php"); ?>
+    <!-- Navbar -->
+  </div>
+  <!--
  -->
   <div class="row" style="margin-top:100px">
-	  <div class="col-xl-3">
-		  <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/php/"; include($IPATH."admin_sidenav.php"); ?> <!-- Admin SideNav -->
-	  </div>
-	  <div class="col-xl-9">
-		  <div class="tab-content">
-      		<div class="tab-pane fade <?php echo ($page == "admininfo"? 'show active':''); ?>">
-		  		      <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/admin/"; include($IPATH."adminInfo.php"); ?> <!-- Admin info -->
-			    </div>
-          <div class="tab-pane fade <?php echo ($page == "userman"? 'show active':''); ?>">
-            <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/admin/"; include($IPATH."adminUserManagement.php"); ?> <!-- Admin User Management -->
-          </div>
-          <div class="tab-pane fade <?php echo ($page == "themes"? 'show active':''); ?>">Themes</div>
-          <div class="tab-pane fade <?php echo ($page == "manageprod"? 'show active':''); ?>">
-            <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/admin/"; include($IPATH."adminProductManagement.php"); ?> <!-- Admin Product Management -->
-          </div>
-		      <div class="tab-pane fade <?php echo ($page == "settings"? 'show active':''); ?>">Settings</div>
-		      <div class="tab-pane fade <?php echo ($page == "carousel"? 'show active':''); ?>">Carousel content</div>
-          <div class="tab-pane fade <?php echo ($page == "documentation"? 'show active':''); ?>">
-              <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/admin/"; include($IPATH."adminDocumentation.php"); ?> <!-- Admin Documentation -->
-          </div>
+    <div class="col-xl-3">
+      <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
+      include($IPATH . "admin_sidenav.php"); ?>
+      <!-- Admin SideNav -->
     </div>
-	  </div>
-</div>
-<script src="js/jquery-3.4.1.min.js"></script>
+    <div class="col-xl-9">
+      <div class="tab-content">
+        <div class="tab-pane fade <?php echo ($page == "admininfo" ? 'show active' : ''); ?>">
+          <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/admin/";
+          include($IPATH . "adminInfo.php"); ?>
+          <!-- Admin info -->
+        </div>
+        <div class="tab-pane fade <?php echo ($page == "userman" ? 'show active' : ''); ?>">
+          <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/admin/";
+          include($IPATH . "adminUserManagement.php"); ?>
+          <!-- Admin User Management -->
+        </div>
+        <div class="tab-pane fade <?php echo ($page == "themes" ? 'show active' : ''); ?>">Themes</div>
+        <div class="tab-pane fade <?php echo ($page == "manageprod" ? 'show active' : ''); ?>">
+          <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/admin/";
+          include($IPATH . "adminProductManagement.php"); ?>
+          <!-- Admin Product Management -->
+        </div>
+        <div class="tab-pane fade <?php echo ($page == "settings" ? 'show active' : ''); ?>">Settings</div>
+        <div class="tab-pane fade <?php echo ($page == "carousel" ? 'show active' : ''); ?>">
+          <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/admin/";
+          include($IPATH . "carouselManagement.php"); ?>
+        </div>
+        <div class="tab-pane fade <?php echo ($page == "documentation" ? 'show active' : ''); ?>">
+          <?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/admin/";
+          include($IPATH . "adminDocumentation.php"); ?>
+          <!-- Admin Documentation -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/jquery-3.4.1.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap-4.4.1.js"></script>
 </body>
+
 </html>
--- admin/adminProductManagement.php
+++ admin/adminProductManagement.php
@@ -1,179 +1,177 @@
-
 <div class="container">
 	<h2>Product management</h2>
-  <hr>
+	<hr>
 
 	<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#addNewProduct">
 		Add new product
 	</button>
 	<div class="modal" id="addNewProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog-lg modal-dialog-centered" style="margin-left:20%; margin-right: 20%" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Add new product</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-			<form>
+		<div class="modal-dialog-lg modal-dialog-centered" style="margin-left:20%; margin-right: 20%" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLongTitle">Add new product</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<form>
 
-      <div class="modal-body">
-				<div class="row">
-    			<div class="col">
-						<label for="newProductName">Product Name</label>
-						<div class="input-group mb-3">
-							<input type="text" class="form-control" id="newProductName" >
+					<div class="modal-body">
+						<div class="row">
+							<div class="col">
+								<label for="newProductName">Product Name</label>
+								<div class="input-group mb-3">
+									<input type="text" class="form-control" id="newProductName">
+								</div>
+							</div>
+							<div class="col">
+								<label for="newProductImage">Product Image Link</label>
+								<div class="input-group mb-3">
+									<div class="input-group-prepend">
+										<span class="input-group-text" id="newProductImage1">🔗</span>
+									</div>
+									<input type="text" class="form-control" id="newProductImage" aria-describedby="newProductImage1">
+								</div>
+							</div>
 						</div>
-    			</div>
-    			<div class="col">
-						<label for="newProductImage">Product Image Link</label>
-		  			<div class="input-group mb-3">
-							<div class="input-group-prepend">
-		    				<span class="input-group-text" id="newProductImage1">🔗</span>
-		  				</div>
-		    			<input type="text" class="form-control" id="newProductImage" aria-describedby="newProductImage1">
-		  			</div>
-    			</div>
-  			</div>
 
-				<label for="newProductDescription">Product Description</label>
-				<div class="input-group">
-  				<textarea id="newProductDescription" class="form-control"></textarea>
-				</div>
-				<br>
-				<label for="newProductSpecs">Product Specifications</label>
-				<div class="input-group">
-  				<textarea id="newProductSpecs" class="form-control"></textarea>
-				</div>
-				<br>
-				<div class="row">
-    			<div class="col">
-						<label for="newProductAmazon">Amazon Link</label>
-		  			<div class="input-group mb-3">
-							<div class="input-group-prepend">
-		    				<span class="input-group-text" id="newProductAmazon1">🔗</span>
-		  				</div>
-		    			<input type="text" class="form-control" id="newProductAmazon" aria-describedby="newProductAmazon1">
-		  			</div>
-					</div>
-					<div class="col">
-						<label for="newProductBestBuy">BestBuy Link</label>
-		  			<div class="input-group mb-3">
-							<div class="input-group-prepend">
-		    				<span class="input-group-text" id="newProductBestBuy1">🔗</span>
-		  				</div>
-		    			<input type="text" class="form-control" id="newProductBestBuy" aria-describedby="newProductBestBuy1">
-		  			</div>
-					</div>
-					<div class="col">
-						<label for="newProductNewegg">Newegg Link</label>
-		  			<div class="input-group mb-3">
-							<div class="input-group-prepend">
-		    				<span class="input-group-text" id="newProductNewegg1">🔗</span>
-		  				</div>
-		    			<input type="text" class="form-control" id="newProductNewegg" aria-describedby="newProductNewegg1">
-		  			</div>
-					</div>
-				</div>
+						<label for="newProductDescription">Product Description</label>
+						<div class="input-group">
+							<textarea id="newProductDescription" class="form-control"></textarea>
+						</div>
+						<br>
+						<label for="newProductSpecs">Product Specifications</label>
+						<div class="input-group">
+							<textarea id="newProductSpecs" class="form-control"></textarea>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col">
+								<label for="newProductAmazon">Amazon Link</label>
+								<div class="input-group mb-3">
+									<div class="input-group-prepend">
+										<span class="input-group-text" id="newProductAmazon1">🔗</span>
+									</div>
+									<input type="text" class="form-control" id="newProductAmazon" aria-describedby="newProductAmazon1">
+								</div>
+							</div>
+							<div class="col">
+								<label for="newProductBestBuy">BestBuy Link</label>
+								<div class="input-group mb-3">
+									<div class="input-group-prepend">
+										<span class="input-group-text" id="newProductBestBuy1">🔗</span>
+									</div>
+									<input type="text" class="form-control" id="newProductBestBuy" aria-describedby="newProductBestBuy1">
+								</div>
+							</div>
+							<div class="col">
+								<label for="newProductNewegg">Newegg Link</label>
+								<div class="input-group mb-3">
+									<div class="input-group-prepend">
+										<span class="input-group-text" id="newProductNewegg1">🔗</span>
+									</div>
+									<input type="text" class="form-control" id="newProductNewegg" aria-describedby="newProductNewegg1">
+								</div>
+							</div>
+						</div>
 
-      </div>
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
-		</form>
+					</div>
+					<div class="modal-footer">
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</div>
+				</form>
 
-    </div>
-  </div>
-</div>
+			</div>
+		</div>
+	</div>
 
 	<hr>
-  <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Preview</th>
-      <th scope="col">Product name</th>
-      <th scope="col">Category</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-  if($_GET['page']=='manageprod'){// Initialize the session
-  session_start();
-  // Include config file
-  require_once "config.php";
-	//set page number
-	if (!isset ($_GET['view']) ) {
-		$view = 1;
-	} else {
-		$view = $_GET['view'];
-	}
-	//get current page
-	$curr_page=$_GET['view'];
-	if($curr_page == null){
-		$curr_page=1;
-	}
-	//define total number of results you want per page
-	//getting data a page
-	$results_per_page = 10;
-	$page_first_result = ($view-1) * $results_per_page;
-	$query = "SELECT p_id, p_name, p_category, p_image FROM products";
-	$result = mysqli_query($link, $query);
-	$number_of_result = mysqli_num_rows($result);
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th scope="col">#</th>
+				<th scope="col">Preview</th>
+				<th scope="col">Product name</th>
+				<th scope="col">Category</th>
+				<th scope="col">Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			if ($_GET['page'] == 'manageprod') { // Initialize the session
+				session_start();
+				// Include config file
+				require_once "config.php";
+				//set page number
+				if (!isset($_GET['view'])) {
+					$view = 1;
+				} else {
+					$view = $_GET['view'];
+				}
+				//get current page
+				$curr_page = $_GET['view'];
+				if ($curr_page == null) {
+					$curr_page = 1;
+				}
+				//define total number of results you want per page
+				//getting data a page
+				$results_per_page = 10;
+				$page_first_result = ($view - 1) * $results_per_page;
+				$query = "SELECT p_id, p_name, p_category, p_image FROM products";
+				$result = mysqli_query($link, $query);
+				$number_of_result = mysqli_num_rows($result);
 
-	//determine the total number of pages available
-	$number_of_page = ceil ($number_of_result / $results_per_page);
-//retrieve the selected results from database
+				//determine the total number of pages available
+				$number_of_page = ceil($number_of_result / $results_per_page);
+				//retrieve the selected results from database
 
-   //retrieve the selected results from database
-   $query = "SELECT p_id, p_name, p_category, p_image FROM products LIMIT " . $page_first_result . ',' . $results_per_page;
-   $result = mysqli_query($link, $query);
+				//retrieve the selected results from database
+				$query = "SELECT p_id, p_name, p_category, p_image FROM products LIMIT " . $page_first_result . ',' . $results_per_page;
+				$result = mysqli_query($link, $query);
 
-   //display the retrieved result on the webpage
-   while ($row = mysqli_fetch_array($result)) {
-       echo '<tr>
-              <th scope="row">'.$row['p_id'].'</th>
-              <td><img src="'.$row['p_image'].'" width="25px"></td>
-              <td>'.$row['p_name'].'</td>
-              <td>'.$row['p_category'].'</td>';
-        echo '<td>
+				//display the retrieved result on the webpage
+				while ($row = mysqli_fetch_array($result)) {
+					echo '<tr>
+              <th scope="row">' . $row['p_id'] . '</th>
+              <td><img src="' . $row['p_image'] . '" width="25px"></td>
+              <td>' . $row['p_name'] . '</td>
+              <td>' . $row['p_category'] . '</td>';
+					echo '<td>
                 <div class="dropdown">
                     <button type="button" class="btn btn-default" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i style="font-size:20px;" class="fa fa-ellipsis-v"></i>
                     </button>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                      <a class="dropdown-item" href="#">Edit product</a>
-                      <a class="dropdown-item" href="#">Delete Product</a>
+                      <a class="dropdown-item" href="/admin/removeproduct.php?userid=' . $row['p_id'] . '">Delete Product</a>
                     </div>
                 </div>
             </td>';
 
-      echo '</tr>';
-   }
-}
+					echo '</tr>';
+				}
+			}
 
 
 
-?>
-</tbody>
-</table>
-<?php
-if($_GET['page']=='manageprod'){
-echo '<div>
+			?>
+		</tbody>
+	</table>
+	<?php
+	if ($_GET['page'] == 'manageprod') {
+		echo '<div>
 				<nav aria-label="Page navigation example">
 					<ul class="pagination justify-content-center">';
-for($page = 1; $page<= $number_of_page; $page++) { //pagination
-	if($curr_page ==$page){
-						 $isActive="active";
-	}
-	else{
-		$isActive="";
-	}
-	echo '<li class="page-item '.$isActive.' "><a class="page-link" href = "admin.php?page=manageprod&view=' . $page . '">' . $page . ' </a></li>'; }
-	echo '</ul>
+		for ($page = 1; $page <= $number_of_page; $page++) { //pagination
+			if ($curr_page == $page) {
+				$isActive = "active";
+			} else {
+				$isActive = "";
+			}
+			echo '<li class="page-item ' . $isActive . ' "><a class="page-link" href = "admin.php?page=manageprod&view=' . $page . '">' . $page . ' </a></li>';
+		}
+		echo '</ul>
 		</nav>
 	</div>';
-}
-?>
+	}
+	?>
 </div>
